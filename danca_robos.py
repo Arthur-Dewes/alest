0c5887781c061720a7c4e1eb8e5b432590e5552c
Arthur-Dewes <arthurgdewes@gmail.com>
2025-03-25 14:23:24 -0300

    não ta bom, mas ta melhor

--- danca_robos.py
+++ danca_robos.py
@@ -1,23 +1,24 @@
-n = 111
-arr = [21, 86, 81, 80, 25, 64, 61, 101, 50, 94, 54, 91, 16, 7, 88, 109, 44, 32, 55, 9, 38, 41, 77, 2, 47, 53, 36, 104, 85, 11, 99, 42, 60, 14, 51, 24, 75, 17, 92, 48, 105, 67, 110, 15, 69, 71, 95, 33, 72, 6, 100, 82, 79, 102, 106, 66, 43, 22, 103, 27, 56, 59, 31, 12, 4, 96, 34, 89, 35, 87, 39, 76, 49, 57, 3, 84, 10, 108, 37, 18, 90, 58, 45, 62, 19, 26, 68, 28, 5, 0, 8, 63, 1, 65, 93, 46, 70, 107, 83, 78, 74, 97, 13, 52, 29, 73, 23, 20, 98, 30, 40]
+import time
+
+n = 101
+tup = (48, 80, 39, 23, 90, 69, 56, 42, 38, 87, 65, 79, 4, 10, 61, 77, 8, 1, 89, 6, 50, 71, 63, 53, 72, 28, 12, 76, 73, 43, 74, 91, 95, 94, 78, 88, 37, 83, 64, 35, 21, 66, 24, 86, 9, 67, 7, 93, 47, 92, 33, 81, 31, 36, 17, 32, 52, 55, 85, 40, 57, 75, 20, 59, 22, 18, 46, 45, 13, 16, 51, 97, 29, 44, 58, 98, 34, 82, 96, 15, 19, 70, 30, 100, 0, 2, 3, 5, 11, 60, 99, 62, 27, 14, 84, 68, 49, 25, 26, 54, 41)
 
 def troca(tup: tuple[int], pos: list[int], n):
     return tuple(tup[pos[i]] for i in range(n))
         
-def danca(arr, n):
-    chave = arr
-    memo = {tuple(arr): 1, tuple(sorted(arr)): 1}
+def danca(tup, n):
+    chave = tup
     count = 1
 
     while True:
-        chave = troca(chave, arr, n)
-        
-        if chave in memo:
-            memo[chave] += 1
-            if memo[chave] > 1:
-                print(memo)
-                return count
+        chave = troca(chave, tup, n)
+
+        if chave == tup:
+            return count
         count += 1
 
 if __name__ == '__main__':
-    print(danca(arr, n))
+    start = time.time()
+    print(danca(tup, n))
+    stop = time.time()
+    print(stop - start)
